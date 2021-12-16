d7785486c38048da648253a6b515f487063e251e
elcanion <beethovenssixth@gmail.com>
2021-12-16 14:57:56 -0300

    styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import theme from '../src/theme'
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
+{
+  console.log('hehe')
+}
+    </>
+  )
 }
 
 export default MyApp
