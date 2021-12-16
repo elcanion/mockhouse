73c963d30056453d4fbad16d361855c3072a6a56
elcanion <beethovenssixth@gmail.com>
2021-12-16 18:45:21 -0300

    accessibility

--- src/components/AccessibilityMenu.js
+++ src/components/AccessibilityMenu.js
@@ -1,16 +1,34 @@
-import { ChevronDownIcon } from "@chakra-ui/icons"
-import { Button, IconButton, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
+import { ChevronDownIcon, CloseIcon, HamburgerIcon } from "@chakra-ui/icons"
+import { Button, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, useDisclosure } from "@chakra-ui/react"
 import { ContrastSwitch } from "./ContrastSwitch"
 import { DarkModeSwitch } from "./DarkModeSwitch"
-import { MdAccessibility } from 'react-icons/md'
+import { MdAccessibility, MdAccessibilityNew } from 'react-icons/md'
 
 export const AccessibilityMenu = () => {
+    const { isOpen, onToggle } = useDisclosure();
     return (
-        <Menu>
+        <>
+        <Flex
+        flex={{ base: 1, md: 'auto' }}
+        ml={{ base: -2 }}
+        display={{ base: 'flex', md: 'none' }}>
+        <IconButton
+          onClick={onToggle}
+          icon={
+            isOpen ? <CloseIcon w={3} h={3} /> : <MdAccessibilityNew w={10} h={10} />
+          }
+          variant={'ghost'}
+          aria-label={'Toggle Accessibility Navigation'}
+        />
+      </Flex>
+        
+        {/*<Menu>
             <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
                 Acessibilidade
             </MenuButton>
         </Menu>
+        */}
+        </>
     )
 }
 
