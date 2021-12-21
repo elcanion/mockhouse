cddddb84ddc29b4530fd3d7ccbedcc89eccda308
elcanion <beethovenssixth@gmail.com>
2021-12-20 21:25:05 -0300

    styling

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,14 @@ import 'firebase/compat/auth'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-}
+    apiKey: "AIzaSyArnvEKwGNxEhLaq0wC_WB7VPi4t1kIIAg", 
+    authDomain: "siad-69fc4.firebaseapp.com",  
+    projectId: "siad-69fc4",  
+    storageBucket: "siad-69fc4.appspot.com",  
+    messagingSenderId: "1059746105248",  
+    appId: "1:1059746105248:web:aa058350c003aed2b246c5"  
+  };
+  
 
 firebase.initializeApp(firebaseConfig);
 var auth = firebase.auth();
